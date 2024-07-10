91f03496026661f2e4adee25fba663a19a6d2023
James McArthur <77163783+jamezmca@users.noreply.github.com>
2024-07-11 11:34:54 +1200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -37,16 +37,17 @@ const HomePage = () => {
     { name: 'YouTube', icon: 'fa-brands fa-youtube', link: 'https://www.youtube.com/smoljames?sub_confirmation=1', target: '_blank' },
     { name: 'Roadmap', icon: 'fa-solid fa-signs-post', link: '/roadmap', target: '' },
     // { name: 'Discord', icon: 'fa-brands fa-discord', link: '/discord' },
-    { name: 'Discord', icon: 'fa-brands fa-discord', link: 'https://discord.gg/BYr6gujs4k', target: '_blank' },
     { name: 'Courses', icon: 'fa-solid fa-graduation-cap', link: 'https://www.udemy.com/course/the-complete-javascript-course-zero-to-hero/?referralCode=F6229ABBDBD16EB43FA4', target: '_blank' },
+    { name: 'GitHub', icon: 'fa-solid fa-heart', link: 'https://github.com/jamezmca', target: '_blank' },
+    { name: 'Discord', icon: 'fa-brands fa-discord', link: 'https://discord.gg/BYr6gujs4k', target: '_blank' },
     { name: 'Notes', icon: 'fa-solid fa-book', link: '/notes', target: '' },
-    { name: 'Coaching', icon: 'fa-solid fa-heart', link: '/coaching', target: '' },
   ]
 
   let extras = [
     { name: 'Newsletter', icon: 'fa-regular fa-newspaper', link: 'https://smoljames.beehiiv.com/subscribe', target: '_blank' },
     { name: 'Blog', icon: 'fa-solid fa-bookmark', link: '/blog', target: '' },
     { name: 'My Story', icon: 'fa-solid fa-user-graduate', link: 'https://youtu.be/nP0-oYGiqts', target: '_blank' },
+    { name: 'Coaching', icon: 'fa-solid fa-heart', link: '/coaching', target: '' },
     // { name: 'Patreon', icon: 'fa-brands fa-patreon', link: '/', target: '_blank' },
     { name: 'Portfolio', icon: 'fa-solid fa-lemon', link: '/portfolio', target: '' },
     // { name: 'Store', icon: 'fa-solid fa-store', link: 'https://store.smoljames.com/', target: '_blank' },
