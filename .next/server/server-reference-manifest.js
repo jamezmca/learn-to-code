f2d0a625c7ccaa8deffee73ee2d509ab300b2f45
James McArthur <77163783+jamezmca@users.noreply.github.com>
2025-05-03 11:01:52 +1200

    remove .next/

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ra+fvVXPmxY2brwVsMlX67iZ3ChFVMpBwCL1dV5tucA=\"\n}"
---
+++ app/todo/page.js
@@ -0,0 +1,10 @@
+import MainWrapper from '@/components/MainWrapper'
+import React from 'react'
+
+export default function TodoPage() {
+    return (
+        <MainWrapper>
+            
+        </MainWrapper>
+    )
+}
